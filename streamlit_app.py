5c9a4ccbfcf50c8f61e4c89839010167b9a444e2
Brandon Usuga <77641046+busugaacevedo@users.noreply.github.com>
2022-07-06 18:13:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to the first APP of CAROLINA!
+# Welcome to the first APP!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
